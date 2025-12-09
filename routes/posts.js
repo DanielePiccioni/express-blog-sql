c824ac13a6b5d81b1c6e4f43912948ce04b245a3
DanielePiccioni <danielepiccioni1@gmail.com>
2025-12-09 17:06:38 +0100

    delete posts with destroy and verify on postman

--- routes/posts.js
+++ routes/posts.js
@@ -12,4 +12,21 @@ router.get("/", (req, res) => {
     });
 });
 
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const sql = "DELETE FROM posts WHERE id = ?";
+
+    connection.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error("Errore query DELETE:", err);
+            return res.status(500).json({ error: "Errore del server" });
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: "Post non trovato" });
+        }
+        res.status(204).send();
+    });
+});
+
 module.exports = router;
